c8e98495b2809cb83c635116449c840ff8ec01e0
Philipp Wolfer <phil@parolu.io>
2019-05-03 10:48:55 +0200

    Added rustdoc documentation

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,12 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+//! `discid` allows you to calculate MusicBrainz or FreeDB disc IDs for audio CDs.
+//!
+//! `discid` provides bindings to the MusicBrainz [libdiscid](https://musicbrainz.org/doc/libdiscid)
+//! library. In addition to calculating the disc IDs you can also get advanced data from the
+//! audio CD such as MCN (media catalogue number) and per-track ISRCs.
+
 #![deny(
     // missing_docs,
     missing_debug_implementations,
@@ -58,6 +64,9 @@ extern "C" {
 }
 
 bitflags! {
+    /// Constants representing the features supported by libdiscid.
+    ///
+    /// See `DiscId::read_features()` for details.
     pub struct Features: u32 {
         const READ = 1 << 0;
         const MCN  = 1 << 1;
@@ -66,6 +75,10 @@ bitflags! {
     }
 }
 
+/// `DiscId` holds information about a disc (TOC, MCN, ISRCs).
+///
+/// Use `DiscId::read`, `DiscId::read_features` or `DiscId::put` to initialize
+/// an instance of `DiscId`.
 pub struct DiscId {
     disc: *const size_t,
 }
@@ -76,11 +89,61 @@ impl DiscId {
         DiscId { disc }
     }
 
+    /// Read the disc in the given CD-ROM/DVD-ROM drive extracting only the TOC.
+    ///
+    /// This function reads the disc in the drive specified by the given device
+    /// identifier. If the device is `None`, the default device, as returned by
+    /// `DiscId::default_device()`, is used.
+    ///
+    /// This function will only read the TOC, hence only the disc ID itself will be
+    /// available. Use `DiscId::read_features()` if you want to read also MCN and ISRCs.
+    ///
+    /// # Examples
+    ///
+    /// Read from default device
+    /// ```ignore
+    /// use discid::DiscId;
+    ///
+    /// let disc = DiscId::read(None).expect("Reading disc failed");
+    /// println!("ID: {}", disc.id());
+    /// ```
+    ///
+    /// Read from specific device
+    /// ```ignore
+    /// # use discid::DiscId;
+    /// let disc = DiscId::read(Some("/dev/sr1")).expect("Reading disc failed");
+    /// println!("ID: {}", disc.id());
+    /// ```
     pub fn read(device: Option<&str>) -> Result<DiscId, String>
     {
         DiscId::read_features(device, Features::READ)
     }
 
+    /// Read the disc in the given CD-ROM/DVD-ROM drive with additional features.
+    ///
+    /// This function is similar to `DiscId::read()` but allows to read information about MCN
+    /// and per-track ISRCs in addition to the normal TOC data.
+    ///
+    /// The parameter `features` accepts a bitwise combination of values defined in `Features`.
+    /// `Features::READ` is always implied, so it is not necessary to specify it.
+    ///
+    /// Reading MCN and ISRCs is not available on all platforms. You can use
+    /// `DiscId::has_feature()` to check if a specific feature is available. Passing unsupported
+    /// features here will just be ignored.
+    ///
+    /// Note that reading MCN and ISRC data is significantly slower than just reading the TOC, so
+    /// only request the features you actually need.
+    ///
+    /// # Examples
+    ///
+    /// Read both ISRC and MCN from default device
+    /// ```ignore
+    /// use discid::{DiscId, Features};
+    ///
+    /// let features = Features::MCN | Features::ISRC;
+    /// let disc = DiscId::read_features(None, features).expect("Reading disc failed");
+    /// println!("ID: {}", disc.id());
+    /// ```
     pub fn read_features(device: Option<&str>, features: Features) -> Result<DiscId, String>
     {
         let disc = DiscId::new();
@@ -98,6 +161,25 @@ impl DiscId {
         }
     }
 
+    /// Provides the TOC of a known CD.
+    ///
+    /// This function may be used if the TOC has been read earlier and you want to calculate
+    /// the disc ID afterwards, without accessing the disc drive.
+    ///
+    /// # Examples:
+    ///
+    /// ```
+    /// use discid::DiscId;
+    ///
+    /// let first_track = 1;
+    /// // The offsets contain the total number of sectors as first element, followed by
+    /// // the start sectors of each track.
+    /// let offsets = [
+    ///    242457, 150, 44942, 61305, 72755, 96360, 130485, 147315, 164275, 190702, 205412, 220437,
+    /// ];
+    /// let disc = DiscId::put(first_track, &offsets).expect("DiscId::put() failed");
+    /// assert_eq!("lSOVc5h6IXSuzcamJS1Gp4_tRuA-", disc.id());
+    /// ```
     pub fn put(first: i32, offsets: &[i32]) -> Result<DiscId, String> {
         let disc = DiscId::new();
         let last = (offsets.len() - 1) as c_int;
@@ -109,16 +191,49 @@ impl DiscId {
         }
     }
 
+
+    /// Check if a certain feature is implemented on the current platform.
+    ///
+    /// See the [libdiscid feature matrix](https://musicbrainz.org/doc/libdiscid#Feature_Matrix)
+    /// for a list of supported features per platform.
+    ///
+    /// # Examples
+    /// ```
+    /// use discid::{DiscId, Features};
+    ///
+    /// let can_read = DiscId::has_feature(Features::READ);
+    /// assert!(can_read);
+    /// ```
     pub fn has_feature(feature: Features) -> bool {
         let result = unsafe { discid_has_feature(feature.bits()) };
         result == 1
     }
 
+    /// Return version information about libdiscid.
+    ///
+    /// The returned string will be e.g. `"libdiscid 0.6.2"`.
+    ///
+    /// # Examples
+    /// ```
+    /// use discid::DiscId;
+    ///
+    /// println!("{}", DiscId::version_string());
+    /// ```
     pub fn version_string() -> String {
         let str_ptr = unsafe { discid_get_version_string() };
         to_str(str_ptr)
     }
 
+    /// Return the name of the default disc drive for this operating system.
+    ///
+    /// The default device is system dependent, e.g. `/dev/cdrom` on Linux and `D:` on Windows.
+    ///
+    /// # Examples
+    /// ```
+    /// use discid::DiscId;
+    ///
+    /// println!("{}", DiscId::default_device());
+    /// ```
     pub fn default_device() -> String {
         let version_ptr = unsafe { discid_get_default_device() };
         to_str(version_ptr)
@@ -129,51 +244,62 @@ impl DiscId {
         to_str(str_ptr)
     }
 
+    /// The MusicBrainz disc ID.
     pub fn id(&self) -> String {
         let str_ptr = unsafe { discid_get_id(self.disc) };
         to_str(str_ptr)
     }
 
+    /// The FreeDB disc ID.
     pub fn freedb_id(&self) -> String {
         let str_ptr = unsafe { discid_get_freedb_id(self.disc) };
         to_str(str_ptr)
     }
 
+    /// Return a string representing CD Table Of Contents (TOC).
     pub fn toc_string(&self) -> String {
         let str_ptr = unsafe { discid_get_toc_string(self.disc) };
         to_str(str_ptr)
     }
 
+    /// An URL for submitting the DiscID to MusicBrainz.
     pub fn submission_url(&self) -> String {
         let str_ptr = unsafe { discid_get_submission_url(self.disc) };
         to_str(str_ptr)
     }
 
+    /// The number of the first track on this disc.
     pub fn first_track_num(&self) -> i32 {
         unsafe { discid_get_first_track_num(self.disc) }
     }
 
+    /// The number of the last track on this disc.
     pub fn last_track_num(&self) -> i32 {
         unsafe { discid_get_last_track_num(self.disc) }
     }
 
+    /// The length of the disc in sectors.
     pub fn sectors(&self) -> i32 {
         unsafe { discid_get_sectors(self.disc) }
     }
 
+    /// Returns the offset in sectors for a certain track number (starting at 1).
     pub fn track_offset(&self, track_num: i32) -> i32 {
         unsafe { discid_get_track_offset(self.disc, track_num) }
     }
 
+    /// Returns the length (in sectors) for a certain track number (starting at 1).
     pub fn track_length(&self, track_num: i32) -> i32 {
         unsafe { discid_get_track_length(self.disc, track_num) }
     }
 
+    /// The media catalogue number on the disc, if present.
     pub fn mcn(&self) -> String {
         let str_ptr = unsafe { discid_get_mcn(self.disc) };
         to_str(str_ptr)
     }
 
+    /// Returns the ISRC for a certain track number (starting at 1).
     pub fn track_isrc(&self, track_num: i32) -> String {
         let str_ptr = unsafe { discid_get_track_isrc(self.disc, track_num) };
         to_str(str_ptr)
