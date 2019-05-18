cd8c60050b6736fba00ed5882c9e195fe017632e
Philipp Wolfer <phil@parolu.io>
2019-05-18 15:56:09 +0200

    Improved documentation for DiscId::parse()

--- src/lib.rs
+++ src/lib.rs
@@ -251,7 +251,9 @@ impl DiscId {
         }
     }
 
-    /// Parses a TOC string and returns a [Disc] instance for it.
+    /// Parses a TOC string and returns a `DiscId` instance for it.
+    ///
+    /// The TOC string provided here must have the same format as returned by `DiscId::toc_string`.
     ///
     /// This function can be used if you already have a TOC string like e.g.
     /// `1 11 242457 150 44942 61305 72755 96360 130485 147315 164275 190702 205412 220437`.
